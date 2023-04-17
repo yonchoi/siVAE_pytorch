ed8852bc534b596042c7fa0abc88b28d7169a442
yonchoi <yonchoi@ucdavis.edu>
2023-04-16 17:00:16 -0700

    Bug fix also output nn x
    Bug fix also output nn x

--- siVAEtorch/model/ProbabilityNN.py
+++ siVAEtorch/model/ProbabilityNN.py
@@ -74,7 +74,8 @@ class ProbabilityNN(nn.Module):
         x = nn_output.x
         h = nn_output.h
 
-        output = ModelOutput(h = h)
+        output = ModelOutput(h = h
+                             x = x,)
 
         if return_hidden_only:
 
